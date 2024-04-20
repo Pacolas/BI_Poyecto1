932e2742a079960ff1e5563c7c38bfd79e59beda
Carlos Muñoz <c.munoza@uniandes.edu.co>
2024-04-20 16:14:17 -0500

    Avances front

--- backend_old/model/predict.py
+++ backend_old/model/predict.py
@@ -2,6 +2,7 @@ import joblib
 import backend_old.model.preprocessing as prep
 
 def predict(dataframe, version):
+    prep.download_nltk_resources()
     prep.predict_process(dataframe)
     model = joblib.load( "./backend_old/model/ml_models/trained/"+ version +'.pkl')
     return model.predict(dataframe['words'])
--- frontend/src/components/training/data.js
+++ frontend/src/components/training/data.js
@@ -6,9 +6,33 @@ import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import { blue, green } from '@mui/material/colors';
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Unstable_Grid2';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
 function UploadCSV() {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const [file, setFile] = useState(null);
   const [version, setVersion] = useState('');
+  const [image, setImage] = useState("null");
+  const [precision, setPrecision] = useState('-');
+  const [accuracy, setAccuracy] = useState('-');
+  const [recall, setRecall] = useState('-');
+  const [f1, setF1] = useState('-');
+
+
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      '&:hover': {
+        bgcolor: green[700],
+      },
+    }),
+  };
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -18,26 +42,61 @@ function UploadCSV() {
     setVersion(event.target.value);
   };
 
+
+
+
   const handleSubmit = async () => {
+
+
     const formData = new FormData();
     formData.append('csv_file', file);
     formData.append('version', version);
 
     try {
+      setSuccess(false);
+      setLoading(true);
       await axios.post('http://localhost:8000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       });
+      setSuccess(true);
+      setLoading(false);
       alert('Archivo CSV enviado exitosamente.');
+
+
+      const matrix = await fetch('http://localhost:8000/metrics/' + version + '/matrix');
+      const blob = await matrix.blob();
+      const imageUrl = URL.createObjectURL(blob);
+      setImage(imageUrl);
+
+      fetch('http://localhost:8000/metrics/' + version)
+        .then(function (response) {
+          return response.json();
+        }).then(function (data) {
+          setAccuracy(Math.round(data[0].percent * 1000) / 1000)
+          setPrecision(Math.round(data[1].percent * 1000) / 1000)
+          setRecall(Math.round(data[2].percent * 1000) / 1000)
+          setF1(Math.round(data[3].percent * 1000) / 1000)
+          console.log(data);
+        });
+
+
+
+
+
+
     } catch (error) {
-      console.error('Error al enviar el archivo CSV:', error);
-      alert('Error al enviar el archivo CSV.');
+      setLoading(false);
+      console.error('Error al modelar:', error);
+      alert('Error al realizar el modelo.');
     }
   };
 
+
+
   return (
-    <Box mx paddingLeft="40px">
+    <Box mx>
       <Stack>
         <Typography variant="h4" sx={{ fontWeight: 700 }} textAlign="center">
           Entrena un nuevo modelo
@@ -52,14 +111,152 @@ function UploadCSV() {
           Escribe el nombre con el que deseas guardar el archivo
         </Typography>
         <Box mx="auto" display="flex" alignItems="center">
-          <TextField size="small" type="text" value={version} onChange={handleVersionChange} placeholder="Ingrese el nombre de la version" />
-          <Button size="large" variant="contained" onClick={handleSubmit}>Enviar CSV</Button>
+          <TextField size="small" type="text" value={version} onChange={handleVersionChange} placeholder="Ingrese la versión" />
+          <Box paddingLeft="10px">
+            <Button size="large" variant="contained" disabled={loading} onClick={handleSubmit} sx={buttonSx}> Entrenar modelo </Button>
+          </Box>
+          {loading && (
+            <Box paddingLeft="10px" paddingTop="10px">
+              <CircularProgress
+                size={24}
+                sx={{
+                  color: green[500],
+                }}
+              />
+            </Box>
+          )}
         </Box>
       </Stack>
+      <Grid container spacing={2} columns={16} paddingTop="10px">
+        <Grid xs={8}>
+          <Typography variant="h6" textAlign="center">
+            Métricas
+          </Typography>
+          <Grid container spacing={1} columns={16} paddingTop="10px" sx={{
+            justifyContent: "center",
+            alignContent: "center",
+            display: "flex"
+          }}>
+            <Grid sx={{
+              xs: 8,
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex"
+            }}>
+              <Card sx={{
+                width: "150px",
+                height: "150px",
+                background: "#eeeee4",
+                justifyContent: "center",
+                alignContent: "center",
+                display: "flex"
+              }}>
+                <CardContent>
+                  <Typography variant="h6" textAlign="Center">
+                    {precision}
+                  </Typography>
+                  <Typography variant="h6" textAlign="Center">
+                    Precisión
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid sx={{
+              xs: 8,
+
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex"
+            }}>
+              <Card sx={{
+                width: "150px",
+                height: "150px",
+                background: "#eeeee4",
+                justifyContent: "center",
+                alignContent: "center",
+                display: "flex"
+              }}>
+                <CardContent>
+                  <Typography variant="h6" textAlign="Center">
+                    {accuracy}
+                  </Typography>
+                  <Typography variant="h6" textAlign="Center">
+                    Exactitud
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+
+            <Grid sx={{
+              xs: 8,
+
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex"
+            }}>
+              <Card sx={{
+                width: "150px",
+                height: "150px",
+                background: "#eeeee4",
+                justifyContent: "center",
+                alignContent: "center",
+                display: "flex"
+              }}>
+                <CardContent>
+                  <Typography variant="h6" textAlign="Center">
+                    {recall}
+                  </Typography>
+                  <Typography variant="h6" textAlign="Center">
+                    Recall
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+
+            <Grid sx={{
+              xs: 8,
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex"
+            }}>
+              <Card sx={{
+                width: "150px",
+                height: "150px",
+                background: "#eeeee4",
+                justifyContent: "center",
+                alignContent: "center",
+                display: "flex"
+              }}>
+                <CardContent>
+                  <Typography variant="h6" textAlign="Center">
+                    {f1}
+                  </Typography>
+                  <Typography variant="h6" textAlign="Center">
+                    F1
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+          </Grid>
 
 
 
-      <Training />
+        </Grid>
+        <Grid xs={8}>
+          <Typography variant="h6" textAlign="center">
+            Matriz de confusión
+          </Typography>
+          <Box mx="auto" display="flex" alignItems="center" justifyContent="center">
+            <img src={image} alt="Imagen" width="500px" />
+          </Box>
+
+
+        </Grid>
+      </Grid>
     </Box>
   );
 }
